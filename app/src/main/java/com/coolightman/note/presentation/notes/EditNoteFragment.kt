cce1aaefa99d0081924810d094242bc35e7c7498
Coolightman <drugak48@gmail.com>
2022-02-01 17:12:57 +0300

    add back button

--- app/src/main/java/com/coolightman/note/presentation/notes/EditNoteFragment.kt
+++ app/src/main/java/com/coolightman/note/presentation/notes/EditNoteFragment.kt
@@ -1,31 +1,45 @@
 package com.coolightman.note.presentation.notes
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.coolightman.note.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.coolightman.note.databinding.FragmentEditNoteBinding
 
 class EditNoteFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = EditNoteFragment()
-    }
-
     private lateinit var viewModel: EditNoteViewModel
+
+    private var _binding: FragmentEditNoteBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_edit_note, container, false)
+    ): View {
+        _binding = FragmentEditNoteBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this)[EditNoteViewModel::class.java]
+
+        setListeners()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun setListeners() {
+        binding.btBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 
 }
