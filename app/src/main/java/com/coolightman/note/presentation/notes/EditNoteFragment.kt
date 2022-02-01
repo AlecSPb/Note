fb457dfbda042b5a8ce9f8a337249a913c40677e
Coolightman <drugak48@gmail.com>
2022-02-01 20:56:24 +0300

    edit layout

--- app/src/main/java/com/coolightman/note/presentation/notes/EditNoteFragment.kt
+++ app/src/main/java/com/coolightman/note/presentation/notes/EditNoteFragment.kt
@@ -1,9 +1,11 @@
 package com.coolightman.note.presentation.notes
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
@@ -28,7 +30,16 @@ class EditNoteFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         viewModel = ViewModelProvider(this)[EditNoteViewModel::class.java]
 
+        showKeyboard()
         setListeners()
+    }
+
+    private fun showKeyboard() {
+        val editText = binding.etNoteDescription
+        editText.requestFocus()
+        val inputMethManager =
+            requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        inputMethManager.showSoftInput(editText, InputMethodManager.SHOW_IMPLICIT)
     }
 
     override fun onDestroyView() {
