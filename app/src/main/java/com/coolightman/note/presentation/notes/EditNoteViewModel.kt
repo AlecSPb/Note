b0042ab3f032ddc2b461c5cddac6524c97b579c6
Coolightman <drugak48@gmail.com>
2022-02-02 20:33:48 +0300

    update edit note view model

--- app/src/main/java/com/coolightman/note/presentation/notes/EditNoteViewModel.kt
+++ app/src/main/java/com/coolightman/note/presentation/notes/EditNoteViewModel.kt
@@ -1,7 +1,36 @@
 package com.coolightman.note.presentation.notes
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.coolightman.note.domain.entity.Note
+import com.coolightman.note.domain.repository.NoteRepository
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
 
-class EditNoteViewModel : ViewModel() {
-    
+class EditNoteViewModel(
+    private val repository: NoteRepository
+) : ViewModel() {
+
+    private val handler = CoroutineExceptionHandler { _, throwable ->
+        Log.e("Coroutine_exception", throwable.stackTraceToString())
+    }
+
+    private val _note = MutableLiveData<Note>()
+    val note: LiveData<Note> = _note
+
+    fun fetchNote(noteId: Long) {
+        viewModelScope.launch(handler) {
+            _note.postValue(repository.getNote(noteId))
+        }
+    }
+
+    fun saveNote(note: Note) {
+        viewModelScope.launch(handler) {
+            repository.insertNote(note)
+        }
+    }
+
 }
--- app/src/main/java/com/coolightman/note/presentation/notes/NotesViewModel.kt
+++ app/src/main/java/com/coolightman/note/presentation/notes/NotesViewModel.kt
@@ -1,19 +1,14 @@
 package com.coolightman.note.presentation.notes
 
-import android.util.Log
-import androidx.lifecycle.*
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
 import com.coolightman.note.domain.entity.Note
 import com.coolightman.note.domain.repository.NoteRepository
-import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.launch
 
 class NotesViewModel(
     private val repository: NoteRepository
 ) : ViewModel() {
-
-    private val handler = CoroutineExceptionHandler { _, throwable ->
-        Log.e("Coroutine_exception", throwable.stackTraceToString())
-    }
 
     val notes: LiveData<List<Note>> = liveData {
         emitSource(repository.getAllNotes())
