740223f0ba1124959aec4408c239bf2fcf6ba299
Coolightman <drugak48@gmail.com>
2022-02-11 14:35:16 +0300

    update

--- app/src/main/java/com/coolightman/note/util/MediatorLiveDataExtension.kt
+++ app/src/main/java/com/coolightman/note/util/MediatorLiveDataExtension.kt
@@ -5,23 +5,10 @@ import androidx.lifecycle.MediatorLiveData
 import com.coolightman.note.data.database.dbModel.TaskDb
 
 fun MediatorLiveData<List<TaskDb>>.chainData(
-    list1: LiveData<List<TaskDb>>,
-    list2: LiveData<List<TaskDb>>,
-    list3: LiveData<List<TaskDb>>,
-    list4: LiveData<List<TaskDb>>,
+    vararg lists: LiveData<List<TaskDb>>
 ): MediatorLiveData<List<TaskDb>> {
-
-    this.addSource(list1) {
-        value = combineLists(list1, list2, list3, list4)
-    }
-    this.addSource(list2) {
-        value = combineLists(list1, list2, list3, list4)
-    }
-    this.addSource(list3) {
-        value = combineLists(list1, list2, list3, list4)
-    }
-    this.addSource(list4) {
-        value = combineLists(list1, list2, list3, list4)
+    for (list in lists) {
+        this.addSource(list) { value = combineLists(*lists) }
     }
     return this
 }
@@ -31,7 +18,7 @@ private fun combineLists(vararg lists: LiveData<List<TaskDb>>): List<TaskDb> {
 
     for (list in lists) {
         val value = list.value
-        value?.let { it.forEach { taskDb -> result.add(taskDb) } }
+        value?.let { it.forEach { item -> result.add(item) } }
     }
 
     return result
