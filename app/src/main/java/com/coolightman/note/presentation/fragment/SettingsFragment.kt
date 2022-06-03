e0462991a518deb23fcb4280c725da7d30dfcd75
Coolightman <drugak48@gmail.com>
2022-06-03 14:22:48 +0300

    add export and import fun

--- app/src/main/java/com/coolightman/note/presentation/fragment/SettingsFragment.kt
+++ app/src/main/java/com/coolightman/note/presentation/fragment/SettingsFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -86,11 +87,11 @@ class SettingsFragment : Fragment() {
     }
 
     private fun exportAppData() {
-        makeSnackbar(binding.root, "Try to export app data")
+        Toast.makeText(requireActivity(), "Try to export app data", Toast.LENGTH_SHORT).show()
     }
 
     private fun importAppData() {
-        makeSnackbar(binding.root, "Try to import app data")
+        Toast.makeText(requireActivity(), "Try to import app data", Toast.LENGTH_SHORT).show()
     }
 
     private fun setNoteTitleColor() {
